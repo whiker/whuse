8567d146e599d455051783bb0574a1e287c0f82e
whiker <whiker@163.com>
2015-11-15 16:08:07 +0800

    add mysql

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 clear
 rm -f a.out
-g++ -I. -L./leveldb main.cc -lleveldb -lpthread
+g++ -I. \
+	-L./leveldb -L./mysql \
+	main.cc \
+	-lmysqlclient -lleveldb -lpthread
 if [ -f "a.out" ]; then
 	./a.out
 fi
