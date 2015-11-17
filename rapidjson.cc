fa56f99fc32fd08eca3800cbbcede924ca565677
whiker <whiker@163.com>
2015-11-17 21:19:37 +0800

    rapidjson array

--- rapidjson.cc
+++ rapidjson.cc
@@ -9,6 +9,7 @@ using namespace rapidjson;
 
 int main()
 {
+	// 解析
 	string json_str, line;
 	ifstream fin("a.json");
 	while (fin >> line)
@@ -17,14 +18,31 @@ int main()
 	
 	Document src;
 	src.Parse(json_str.c_str());
-	cout << src["array"][2].GetString() << endl;
+	const char *src_str = src["array"][2].GetString();
+	cout << src_str << endl;
 	
+	// 输出
 	Document dst;
 	dst.SetObject();
-	Value val(src["array"][2], dst.GetAllocator());
-	dst.AddMember("t-1", val, dst.GetAllocator());
-	val = 15;
-	dst.AddMember("t-2", val, dst.GetAllocator());
+	Document::AllocatorType& alloc = dst.GetAllocator();
+	
+	Value val(src["array"][2], alloc);
+	dst.AddMember("t-1", val, alloc);
+	
+	val = StringRef(src_str);
+	dst.AddMember("t-2", val, alloc);
+	
+	val = StringRef(src_str, 10);
+	dst.AddMember("t-2", val, alloc);  // 可以重复
+	
+	Value arr(kArrayType);
+	const char *strs[] = { "hello", "world", "!" };
+	for (int i = 0; i < 3; i++)
+	{
+		val = StringRef(strs[i]);
+		arr.PushBack(val, alloc);
+	}
+	dst.AddMember("t-3", arr, alloc);
 	
 	StringBuffer buf(0, 128);
 	Writer<StringBuffer> writer(buf);
--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 clear
 rm -f a.out
+echo '-----------------------------\n\n\n'
 g++ -I. \
 	-L./leveldb \
-	uuid.cc \
+	rapidjson.cc \
 	-luuid -lmysqlclient -lleveldb -lpthread
 if [ -f "a.out" ]; then
 	./a.out
